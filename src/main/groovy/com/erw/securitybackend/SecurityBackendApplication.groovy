5753f48e9b2e99bd89daa2a2e5c2e7728af2a24a
Ethan Whittemore <erw444@gmail.com>
2020-07-19 21:15:36 -0500

    First commit, Add spring security

---
+++ src/main/groovy/com/erw/securitybackend/SecurityBackendApplication.groovy
@@ -0,0 +1,13 @@
+package com.erw.securitybackend
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class SecurityBackendApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(SecurityBackendApplication, args)
+	}
+
+}
